31e80ce65ceb4ce0cbd69ee2493581a69f989979
Julien <julien.alexandre26@gmail.com>
2018-05-23 18:06:12 +0200

    Création des méthodes de calcul.

--- public/index.php
+++ public/index.php
@@ -6,3 +6,23 @@
  * Time: 17:36
  */
 
+require '../src/TimeTravel.php';
+
+$startDate = new DateTime('1985-12-31');
+$endDate = new DateTime('2018-05-23');
+
+$timeTravel = new TimeTravel($startDate, $endDate);
+echo $timeTravel->getTravelInfo();
+
+echo '<br>';
+
+try {
+    echo $timeTravel->findDate(new DateInterval('PT1000000000S'));
+} catch (Exception $e) {
+    echo $e;
+}
+
+$periods = $timeTravel->backToFutureStepByStep(new DateInterval('P1M1W1D'));
+foreach ($periods as $period) {
+    echo $period . '<br>';
+}
--- src/TimeTravel.php
+++ src/TimeTravel.php
@@ -8,5 +8,96 @@
 
 class TimeTravel
 {
+    private $start;
 
+    private $end;
+
+    /**
+     * TimeTravel constructor.
+     *
+     * @param DateTime $start
+     * @param DateTime $end
+     */
+    public function __construct(DateTime $start, DateTime $end)
+    {
+        $this->start = $start;
+        $this->end = $end;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getStart(): DateTime
+    {
+        return $this->start;
+    }
+
+    /**
+     * @param DateTime $start
+     * @return TimeTravel
+     */
+    public function setStart(DateTime $start): TimeTravel
+    {
+        $this->start = $start;
+        return $this;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getEnd(): DateTime
+    {
+        return $this->end;
+    }
+
+    /**
+     * @param DateTime $end
+     * @return TimeTravel
+     */
+    public function setEnd(DateTime $end): TimeTravel
+    {
+        $this->end = $end;
+        return $this;
+    }
+
+    /**
+     * Return interval between 2 DateTime objects.
+     *
+     * @return string
+     */
+    public function getTravelInfo(): string
+    {
+        $interval = ($this->end)->diff($this->start);
+        return "Il y a $interval->y années,
+                    $interval->m mois,
+                    $interval->h heures,
+                    $interval->i minutes et
+                    $interval->s secondes entre les deux dates.";
+    }
+
+    /**
+     * Add interval to the start date.
+     *
+     * @param DateInterval $interval
+     * @return string
+     * @throws Exception
+     */
+    public function findDate(DateInterval $interval): string
+    {
+        $newDate = $this->start->sub($interval)->format('Y-m-d h:i:s');
+        return "La date est : $newDate";
+    }
+
+    /**
+     * @param DateInterval $step
+     */
+    public function backToFutureStepByStep(DateInterval $step)
+    {
+        $result = [];
+        $dates = new DatePeriod($this->start, $step, $this->end);
+        foreach ($dates as $date) {
+            $result[] .= $date->format('M d Y A h:i');
+        }
+        return $result;
+    }
 }
